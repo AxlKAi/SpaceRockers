41081a2e305706b142ee32868df34c0b8270d339
AxlKAi <36095751+AxlKAi@users.noreply.github.com>
2024-09-12 10:04:16 +0300

    ошибка генерации меша

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class MeshGenerator : MonoBehaviour
 {
-
-    private int xSize = 512;
-    private int zSize = 1;
+    private int _xSize = 4;
+    private int _zSize = 2;
+    private float _xLength = 1f;
+    private float _zLength = 1f;
+    private float _zLastPoint = 0f;
+    private int pointsInSector = 6;
 
     private Mesh _mesh;
     private Vector3[] _verrticles;
@@ -22,7 +25,8 @@ public class MeshGenerator : MonoBehaviour
         }
         else
         {
-            xSize = _audioPeer.FrequiencyBandCountGetter;
+            //TODO delete ???
+            // _xSize = _audioPeer.FrequiencyBandCountGetter;
         }
 
         _mesh = new Mesh();
@@ -34,51 +38,45 @@ public class MeshGenerator : MonoBehaviour
         StartCoroutine(CreateNewLine());
     }
 
-    private void Update()
-    {
-
-    }
-
     private IEnumerator CreateNewLine()
     {
         for (int i = 0; i < 100; i++)
         {
-            zSize++;
-            CreateMesh();
-            UpdateMesh();
+            AddNewLineToMesh();
             yield return new WaitForSeconds(.5f);
         }
     }
 
     private void CreateMesh()
     {
-        _verrticles = new Vector3[(xSize + 1) * (zSize + 1)];
+        _verrticles = new Vector3[(_xSize + 1) * (_zSize + 1)];
 
-        for (int i = 0, z = 0; z <= zSize; z++)
+        for (int i = 0, z = 0; z <= _zSize; z++)
         {
-            for (int x = 0; x <= xSize; x++)
+            for (int x = 0; x <= _xSize; x++)
             {
-                _verrticles[i] = new Vector3(x, 0, z);
+                _verrticles[i] = new Vector3(x * _xLength, 0, _zLastPoint + _zLength);
                 i++;
             }
+
+            _zLastPoint += _zLength;
         }
 
         int triangleMultiplier = 0;
         int vertexDisplacment = 0;
-        int pointsInSector = 6;
 
-        _triangles = new int[xSize * zSize * pointsInSector];
+        _triangles = new int[_xSize * _zSize * pointsInSector];
 
-        for (int z = 0; z < zSize; z++)
+        for (int z = 0; z < _zSize; z++)
         {
-            for (int verrticleNumber = 0, x = 0; x < xSize; x++, verrticleNumber++)
+            for (int verrticleNumber = 0, x = 0; x < _xSize; x++, verrticleNumber++)
             {
                 _triangles[triangleMultiplier + 0] = vertexDisplacment;
-                _triangles[triangleMultiplier + 1] = vertexDisplacment + xSize + 1;
+                _triangles[triangleMultiplier + 1] = vertexDisplacment + _xSize + 1;
                 _triangles[triangleMultiplier + 2] = vertexDisplacment + 1;
                 _triangles[triangleMultiplier + 3] = vertexDisplacment + 1;
-                _triangles[triangleMultiplier + 4] = vertexDisplacment + xSize + 1;
-                _triangles[triangleMultiplier + 5] = vertexDisplacment + xSize + 2;
+                _triangles[triangleMultiplier + 4] = vertexDisplacment + _xSize + 1;
+                _triangles[triangleMultiplier + 5] = vertexDisplacment + _xSize + 2;
 
                 _verrticles[vertexDisplacment].y = _audioPeer.FrequiencyBand[verrticleNumber] * 500; //TODO хардкод
 
@@ -89,11 +87,13 @@ public class MeshGenerator : MonoBehaviour
             vertexDisplacment++;
         }
     }
-
     private void AddNewLineToMesh()
     {
         int oldVerticelsArrayLength = _mesh.vertices.Length;
-        int newVerticelsArrayLength = (xSize + 1) * (zSize + 1);
+        int oldTriangleArrayLength = _mesh.triangles.Length;
+
+        _zSize++;
+        int newVerticelsArrayLength = (_xSize + 1) * (_zSize + 1);
 
         if (oldVerticelsArrayLength > newVerticelsArrayLength)
         {
@@ -102,37 +102,44 @@ public class MeshGenerator : MonoBehaviour
         }
 
         _verrticles = new Vector3[newVerticelsArrayLength];
+        _triangles = new int[_xSize * _zSize * pointsInSector];
 
         System.Array.Copy(_mesh.vertices, _verrticles, oldVerticelsArrayLength);
+        System.Array.Copy(_mesh.triangles, _triangles, oldTriangleArrayLength);
 
         // добавляем одну строку в конец архива
-        for (int i = oldVerticelsArrayLength; i <= newVerticelsArrayLength; i++)
+        _zLastPoint += _zLength;
+
+        for (int i = 0; i < _xSize; i++)
         {
-            _verrticles[i] = new Vector3(x, 0, z);
-            i++;
+            _verrticles[oldVerticelsArrayLength + i] = new Vector3(i * _xLength, _audioPeer.FrequiencyBand[i] * 500, _zLastPoint); //TODO хардкод
         }
 
-        for (int verrticleNumber = 0, x = 0; x < xSize; x++, verrticleNumber++)
+        _verrticles[oldVerticelsArrayLength + _xSize] = new Vector3(_xSize * _xLength, 0, _zLastPoint); //TODO хардкод
+
+
+        int index = oldTriangleArrayLength;
+
+        for (int x = 0; x < _xSize; x++)
         {
-            _triangles[triangleMultiplier + 0] = vertexDisplacment;
-            _triangles[triangleMultiplier + 1] = vertexDisplacment + xSize + 1;
-            _triangles[triangleMultiplier + 2] = vertexDisplacment + 1;
-            _triangles[triangleMultiplier + 3] = vertexDisplacment + 1;
-            _triangles[triangleMultiplier + 4] = vertexDisplacment + xSize + 1;
-            _triangles[triangleMultiplier + 5] = vertexDisplacment + xSize + 2;
+            _triangles[index + 0] = oldVerticelsArrayLength + x;
+            _triangles[index + 1] = oldVerticelsArrayLength + x + _xSize + 1;
+            _triangles[index + 2] = oldVerticelsArrayLength + x + 1;
+            _triangles[index + 3] = oldVerticelsArrayLength + x + 1;
+            _triangles[index + 4] = oldVerticelsArrayLength + x + _xSize + 1;
+            _triangles[index + 5] = oldVerticelsArrayLength + x + _xSize + 2;
 
-            _verrticles[vertexDisplacment].y = _audioPeer.FrequiencyBand[verrticleNumber] * 500; //TODO хардкод
-
-            vertexDisplacment++;
-            triangleMultiplier += pointsInSector;
+            index += pointsInSector;
         }
-    }
+
+        UpdateMesh();
+    } 
 
     private void UpdateMesh()
     {
         _mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
 
-        _mesh.Clear();
+        // _mesh.Clear();
 
         _mesh.vertices = _verrticles;
         _mesh.triangles = _triangles;
