22f1067ed8dfb1c7749792e38fb7659a9e082565
AxlKAi <36095751+AxlKAi@users.noreply.github.com>
2024-09-20 14:58:18 +0300

    save

---
+++ Assets/Scripts/AudioPolylineBezier.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class AudioPolylineBezier : MonoBehaviour
+{
+    [SerializeField]
+    private AudioPeer _audioPeer;
+    [SerializeField]
+    private float _amplitudeAmpfiler = 500f;
+    [SerializeField]
+    private float _lineLength = 7f;
+    [SerializeField]
+    private float _timeToLive = 4f;
+    [SerializeField]
+    private float _amplitudeDropLength = .3f;
+    [SerializeField]
+    private float _moveSpeed = -2;
+
+    private LineRenderer _lineRenderer;
+    private int _linesCount = 60;
+    private float _positionZ = 0;
+
+    //TODO amplitudeAmpfiler перенести в клас _audioPeer ??
+    public void SetLinePointsFromAudioSource()
+    {
+
+
+        for (int i = 0; i < segmentsCount; i++)
+        {
+            float t = (float)i / (float)(segmentsCount - 1);
+            Vector3 point = CalculateBezierPoint(t, points[0], points[1], points[2], points[3]);
+            render.SetPosition(i, point);
+        }
+
+        for (int i = 0; i < _linesCount; i++)
+        {
+            _lineRenderer.SetPosition(i, new Vector3(i * _lineLength, _audioPeer.Samples[i]*_amplitudeAmpfiler, _positionZ));
+        }
+    }
+
+    public void SetAudioPeer(AudioPeer audioPeer)
+    {
+        _audioPeer = audioPeer;
+    }
+
+    public void SetPositionZ(float positionZ)
+    {
+        _positionZ = positionZ;
+    }
+
+    private void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+
+        if (_lineRenderer == null)
+            Debug.LogError("Cant find LineRenderer component.");
+
+        if (_audioPeer == null)
+            Debug.LogError("Can`t find AudioPeer component.");
+
+        _lineRenderer.positionCount = _linesCount;
+        SetLinePointsFromAudioSource();
+    }
+
+    private void FixedUpdate()
+    {
+        _timeToLive -= Time.fixedDeltaTime;
+
+        if (_timeToLive < 0)
+            Destroy(gameObject);
+
+        AmplitudeDrop();
+    }
+
+    private void AmplitudeDrop()
+    {
+        Vector3[] points = new Vector3[_lineRenderer.positionCount];
+
+        _lineRenderer.GetPositions(points);
+
+        for (int i=0; i<points.Length; i++)
+        {
+            if (points[i].y > 0)
+                points[i].y -= _amplitudeDropLength;
+
+            points[i].z += _moveSpeed; 
+        }
+
+        _lineRenderer.SetPositions(points);
+    }
+
+    private Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * t;
+        Vector3 p = uuu * p0;
+        p += 3 * uu * t * p1;
+        p += 3 * u * tt * p2;
+        p += ttt * p3;
+        return p;
+    }
+
+    /* 
+     * 
+     * 
+     * 
+     */
+}
