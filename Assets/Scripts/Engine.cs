7a35f4bfadefcabe551c451774e58bee7320db2a
AxlKAi <alexey.i.krasnov@gmail.com>
2024-09-27 18:59:54 +0300

    save

--- Assets/Scripts/Engine.cs
+++ Assets/Scripts/Engine.cs
@@ -10,27 +10,33 @@ public class Engine : MonoBehaviour
     [SerializeField] private float _maxDistance;
     [SerializeField] private LayerMask _layerMask;
 
-    private Rigidbody _rigidbody;
+    private Rigidbody _targetBody;
     private Transform _transform;
 
     public void Initialize(Rigidbody targetBody)
     {
         _transform = transform;
-        _rigidbody = targetBody;
+        _targetBody = targetBody;
     }
 
     private void FixedUpdate()
     {
-        if (_rigidbody == null)
+        if (_targetBody == null)
             return;
 
         var forward = _transform.forward;
 
         if( Physics.SphereCast(_transform.position, _spherecastRadius, forward, out RaycastHit hitInfo, _maxDistance, _layerMask, QueryTriggerInteraction.Ignore))
         {
-            var force = -forward * _maxForce * Mathf.Clamp01(1 - hitInfo.distance / 10);
-            _rigidbody.AddForce( force, ForceMode.Impulse);
-            Debug.Log($"force = {force}");
+            float distance = hitInfo.distance;
+            var calcDistance = Mathf.Clamp(distance, _altitude - 10f, _altitude +10f);   // Зажимаем высоту для расчета силы
+
+            var forceFactor  = calcDistance.Remap(_altitude - 10f, _altitude + 10f, _maxForce, 0);  // и ремапим ее на _maxForce
+                                                                    // тут обратить внимание что ремап идет на перевернутые значения, и это прекрасно работате
+
+            var force = -forward * forceFactor; 
+            _targetBody.AddForce( force, ForceMode.Force);
+            Debug.Log($"force = {force}   distance = {calcDistance}   forceFactor = {forceFactor}");
         }
     }
 
