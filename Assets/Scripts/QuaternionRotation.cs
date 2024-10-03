a2d09397c5423fb95aa185e810d9ee8596e7e438
AxlKAi <36095751+AxlKAi@users.noreply.github.com>
2024-10-03 10:54:39 +0300

    экспериментирую с кватернионами

--- Assets/Scripts/QuaternionRotation.cs
+++ Assets/Scripts/QuaternionRotation.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 public class QuaternionRotation : MonoBehaviour
 {
     [SerializeField] private float _angle;
-    [SerializeField] private Vector3 _axis;
+    [SerializeField] private Transform _axis;
 
     private Transform _transform;
+    private float _step = 0;
+    private Quaternion _startRotation;
 
     [ProPlayButton]
     public void Rotate()
     {
         // Метод создает кватернион при умножении на который будет поворот на нужный нам угол
         // вокруг заданной оси. Причем ось может быть любой!!!
-        Quaternion quaternion = Quaternion.AngleAxis(_angle, _axis);
+        Quaternion quaternion = Quaternion.AngleAxis(_angle, _axis.forward);
+        quaternion = Quaternion.Slerp(quaternion, _transform.rotation, Mathf.Clamp01( _step));
+        _transform.rotation = quaternion * _startRotation;
+    }
 
-        _transform.rotation = quaternion * _transform.rotation;
+    private void Update()
+    {
+        _step += .002f;
+        Rotate();
+        Debug.Log($"step = {_step}");
     }
 
     private void Awake()
     {
         _transform = transform;
+        _startRotation = transform.rotation;
     }
 
     private void OnDrawGizmos()
@@ -29,7 +39,7 @@ public class QuaternionRotation : MonoBehaviour
             return;
 
         Gizmos.color = Color.red;
-        Gizmos.DrawRay(transform.position, _axis * 10f);
-        Gizmos.DrawRay(transform.position, -_axis * 10f);
+        Gizmos.DrawRay(transform.position, _axis.forward * 10f);
+        Gizmos.DrawRay(transform.position, -_axis.forward * 10f);
     }
 }
