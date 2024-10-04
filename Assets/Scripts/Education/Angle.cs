d45316dd02fd6686a65e8a50d1925082153a0f94
AxlKAi <36095751+AxlKAi@users.noreply.github.com>
2024-10-04 23:53:37 +0300

    angle between vectors

---
+++ Assets/Scripts/Education/Angle.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Angle : MonoBehaviour
+{
+    [SerializeField] private SimpleVector _vectorOne;
+    [SerializeField] private SimpleVector _vectorTwo;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_vectorOne == null || _vectorTwo == null)
+            return;
+
+        gameObject.name = $"angle = {Vector3.SignedAngle(_vectorOne.Vector, _vectorTwo.Vector, Vector3.up):F2}";
+    }
+}
